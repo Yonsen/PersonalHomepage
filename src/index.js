9d9b42f61f010d7fac2d29fe0b835a37a93cb59b
Yonsen <yonsenwu@hotmail.com>
2017-03-23 18:18:51 +0800

    woo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wu on 2017/2/21.
+ */
+require(__dirname + '/css/index.less');
+var Swiper = require(__dirname + '/lib/swiper.min.js');
+var Clipboard = require(__dirname + '/lib/clipboard.min.js');
+var Util = require(__dirname + '/js/util.js');
+var app = {
+    init: function () {
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            direction: 'vertical',
+            slidesPerView: 1,
+            paginationClickable: true,
+            spaceBetween: 30,
+            mousewheelControl: true
+        });
+    },
+}.init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,12 @@
 const path = require('path'); // 导入路径包
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// Create multiple instances
-const extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
-const extractLESS = new ExtractTextPlugin('stylesheets/[name].less');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './index.js' // 入口文件
+        index: './src/index.js'/*, // 入口文件
+        vendors: ['./lib/jquery-1.7.2.min.js', './lib/swiper.min.js']*/
     },
     // 输出文件 build下的bundle.js
     output: {
@@ -27,19 +25,43 @@ module.exports = {
             {test: /\.css$/, loader: 'style-loader!css-loader'},
             {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
             {test: /\.hbs$/, loader: "handlebars-loader"},
+            {test: /\.html$/, loader: 'html-withimg-loader'},
             {test: /\.(jpg|png|gif)$/, loader: 'url-loader?limit=8192&name=images/[hash:8].[ext]'},
+            {test: /\.(otf|woff|woff2|eot|ttf)$/i, loader: "file-loader?limit=8192&name=fonts/[hash:8].[ext]"}
             /*{test: /\.(jpg|png)$/, loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'},*/
-            {test: /\.(otf|woff|woff2|eot|ttf)$/i, loader: "file-loader?limit=8192&name=[path][name].[ext]"}
+            /*{test: /\.(otf|woff|woff2|eot|ttf)$/i, loader: "file-loader?limit=8192&name=[path][name].[ext]"}*/
         ]
     },
     plugins: [
+        // new webpack.IgnorePlugin(/\.\/jquery-1.7.2.min.js$/),
+        // new webpack.IgnorePlugin(/\.\/swiper.min.js$/),
+        // new ExtractTextPlugin('./css/common.css'),
+        //把入口文件里面的数组打包成vendors.js
+        //new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'vendors.bundle.js'}),
+        /*new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),*/
         /*new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
             "window.Clipboard": "clipboard"
         }),*/
         new HtmlWebpackPlugin({
-            template: './index.html'
+            /*minify: {
+                removeAttributeQuotes: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true
+            },*/
+            template: './src/index.html'
         })
     ]
 };
